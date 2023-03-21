f19933e091ac412453d5dcdc503eabc3f9c4c028
Oleksandr Pavlenko <sashapavlenkouk@gmail.com>
2023-03-21 05:40:40 +0300

    Product version

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,123 @@
-import gulp from "gulp";
-import plumber from "gulp-plumber";
-import postcss from "gulp-postcss";
-import less from "gulp-less";
-import autopefixer from "autoprefixer";
-import browser from "browser-sync";
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import gulpIf from 'gulp-if';
+import less from 'gulp-less';
+import postcss from 'gulp-postcss';
+import postUrl from 'postcss-url';
+import autoprefixer from 'autoprefixer';
+import csso from 'postcss-csso';
+import terser from 'gulp-terser';
+import squoosh from 'gulp-libsquoosh';
+import svgo from 'gulp-svgmin';
+import { stacksvg } from 'gulp-stacksvg';
+import { deleteAsync } from 'del';
+import browser from 'browser-sync';
 
-export const styles = () => {
+let isDevelopment = true;
+
+export function processMarkup() {
+  return gulp.src('source/*.html').pipe(gulp.dest('build'));
+}
+
+export function processStyles() {
   return gulp
-    .src("./source/less/style.less", { sourcemaps: true })
+    .src('source/less/*.less', { sourcemaps: isDevelopment })
     .pipe(plumber())
     .pipe(less())
-    .pipe(postcss([autopefixer()]))
-    .pipe(gulp.dest("./source/css", { sourcemaps: true }))
+    .pipe(postcss([postUrl({ assetsPath: '../' }), autoprefixer(), csso()]))
+    .pipe(gulp.dest('build/css', { sourcemaps: isDevelopment }))
     .pipe(browser.stream());
-};
+}
 
-const server = (done) => {
+export function processScripts() {
+  return gulp
+    .src('source/js/**/*.js')
+    .pipe(terser())
+    .pipe(gulp.dest('build/js'))
+    .pipe(browser.stream());
+}
+
+export function optimizeImages() {
+  return gulp
+    .src('source/img/**/*.{png,jpg}')
+    .pipe(gulpIf(!isDevelopment, squoosh()))
+    .pipe(gulp.dest('build/img'));
+}
+
+export function optimizeVector() {
+  return gulp
+    .src(['source/img/**/*.svg', '!source/img/icons/**/*.svg'])
+    .pipe(svgo())
+    .pipe(gulp.dest('build/img'));
+}
+
+export function createStack() {
+  return gulp
+    .src('./source/img/icons/**/*.svg')
+    .pipe(svgo())
+    .pipe(stacksvg())
+    .pipe(gulp.dest('build/img/icons'));
+}
+
+export function copyAssets() {
+  return gulp
+    .src(
+      [
+        'source/fonts/**/*.{woff2,woff}',
+        'source/*.ico',
+        'source/*.webmanifest',
+      ],
+      {
+        base: 'source',
+      }
+    )
+    .pipe(gulp.dest('build'));
+}
+
+export function startServer(done) {
   browser.init({
     server: {
-      baseDir: "source",
+      baseDir: 'build',
     },
     cors: true,
     notify: false,
     ui: false,
   });
   done();
-};
+}
 
-const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series(styles));
-  gulp.watch("source/*.html").on("change", browser.reload);
-};
+function reloadServer(done) {
+  browser.reload();
+  done();
+}
 
-export default gulp.series(styles, server, watcher);
+function watchFiles() {
+  gulp.watch('source/less/**/*.less', gulp.series(processStyles));
+  gulp.watch('source/script/script.js', gulp.series(processScripts));
+  gulp.watch('source/*.html', gulp.series(processMarkup, reloadServer));
+}
+
+function compileProject(done) {
+  gulp.parallel(
+    processMarkup,
+    processStyles,
+    processScripts,
+    optimizeVector,
+    createStack,
+    copyAssets,
+    optimizeImages
+  )(done);
+}
+
+function deleteBuild() {
+  return deleteAsync('build');
+}
+
+export function buildProd(done) {
+  isDevelopment = false;
+  gulp.series(deleteBuild, compileProject)(done);
+}
+
+export function runDev(done) {
+  gulp.series(deleteBuild, compileProject, startServer, watchFiles)(done);
+}
